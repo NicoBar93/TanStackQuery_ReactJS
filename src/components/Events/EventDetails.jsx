56ef42466359a175be546b7a016682f9847527eb
Nicola Barbaro <barbaronico93@gmail.com>
2025-01-20 17:59:05 +0100

    Completing Event Detail Page

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,79 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import Header from "../Header.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { fetchEvent, deleteEvent } from "../../util/Fetch";
 
-import Header from '../Header.jsx';
+export default function EventDetails({}) {
+  const { mutate, isPending } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    }
+  })
 
-export default function EventDetails() {
+  function handleDelete() {
+    mutate({ id });
+  }
+
+  const params = useParams();
+
+  const id = params.id;
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["event", id],
+    queryFn: () => fetchEvent({id}),
+  });
+
+  // console.log(data);
+
   return (
     <>
       <Outlet />
       <Header>
-        <Link to="/events" className="nav-item">
+        <Link
+          to="/events"
+          className="nav-item"
+        >
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isLoading && <p>Loading data...</p>}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.event.title}</h1>
+            <nav>
+              <button 
+              onClick={handleDelete}
+              >{isPending ? "Deleting..." : 'Delete'}</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img
+              src={`http://localhost:3000/${data.event.image}`}
+              alt={data.event.title}
+            />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.event.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.event.date} @ {data.event.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.event.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
+      {isError && (
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message || "Failed to fetch event datails"}
+        />
+      )}
     </>
   );
 }
